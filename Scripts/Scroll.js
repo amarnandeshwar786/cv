1b93f8687fd9a64fed00d4d138802c9eeb5b375f
Amar Khushal Nandeshwa <34885267+amarnandeshwar786@users.noreply.github.com>
2020-07-09 03:32:05 +0530

    Initial CV Website Source Code

---
+++ Scripts/Scroll.js
@@ -0,0 +1,10 @@
+﻿$(".nav a").click(function (e) {
+    var _left = parseInt($($(this).attr("href")).position().left);
+    $(".content-scroller").animate({ scrollLeft: _left }, 1000, 'easeInOutExpo', function () { });
+	//window.history.pushState(e.target.dataset.tabFor, $("#"+$(this).attr("id")+" span").html(), "amarnandeshwar.com/"+$("#"+$(this).attr("id")+" span").html());
+});
+$(".mobile-nav a").click(function (e) {
+    var _top = parseInt($($(this).attr("href")).position().top);
+    $(".content-scroller").animate({ scrollTop: _top }, 1000, 'easeInOutExpo', function () { });
+	//window.history.pushState(e.target.dataset.tabFor, $("#"+$(this).attr("id")+" span").html(), "amarnandeshwar.com/"+$("#"+$(this).attr("id")+" span").html());
+});
